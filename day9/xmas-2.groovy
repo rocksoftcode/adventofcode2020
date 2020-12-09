63d12ea9ffa39605a14f94c0ce4cd5a524b07759
Rocky DeVries <rocky.devries.contractor@hilton.com>
2020-12-09 11:49:51 -0600

    Day 9

--- day9/xmas-2.groovy
+++ day9/xmas-2.groovy
@@ -1,8 +1,8 @@
 def input = new File('input.txt').text.split('\n').collect { new BigInteger(it) }
 final def INVALID = 32321523
-
-for (i in 0..input.size() -1) {
-	(i..input.size() -1).inject(new BigDecimal(0)) { acc, j ->
+final def LIMIT = input.size() - 1
+for (i in 0..LIMIT) {
+	(i..LIMIT).inject(new BigDecimal(0)) {acc, j ->
 		if (acc == INVALID) {
 			def chunk = input.subList(i, j)
 			println chunk.min() + chunk.max()
