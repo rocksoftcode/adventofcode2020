8895e2f8bf8c2acee089b15c6979f2dc175d98f7
Rocky DeVries <rocky.devries.contractor@hilton.com>
2020-12-10 22:01:20 -0600

    Day 10

--- day10/jolt-2.groovy
+++ day10/jolt-2.groovy
@@ -1,9 +1,8 @@
-def big = { new BigInteger(it) }
-def input = new File('input.txt').text.split('\n').collect {big(it)}.sort()
-def cache = [big(0),big(0),big(1)] + [big(0)] * input.last()
+def input = new File('input.txt').text.split('\n').collect {Integer.parseInt(it)}.sort()
+List<BigInteger> steps = [0, 0, 1] + [0] * input.last()
 input.each {
-	def step = it + big(2)
-	cache[step] = cache[step - big(3)..step].sum()
+	def step = it + 2
+	steps[step] = new BigInteger(steps[step - 3..step].sum().toString())
 }
-println cache.last()
+println steps.last()
 
