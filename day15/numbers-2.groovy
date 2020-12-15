4d49ed1deb3edd855a60342adedb137b64a8f46c
Rocky DeVries <rocky.devries.contractor@hilton.com>
2020-12-15 15:05:48 -0600

    Day 15

--- day15/numbers-2.groovy
+++ day15/numbers-2.groovy
@@ -1,11 +1,10 @@
 def input = [19,0,5,1,10,13]
-def hist = input.withIndex().collectEntries { v, i -> [v, [i+1]] }
-def last = input.last()
-(hist.size()+1..30000000).each {
-    def log = hist[last]
-    def spoken = !log || log.size() == 1 ? 0 : log[-1] - log[-2]
-    if (!hist[spoken]) hist[spoken] = []
-    hist[spoken] << it
-    last = spoken
+def hist = input.withIndex().collectEntries { v, i -> [v, i+1] }
+def index = -1
+def target = input.last()
+for (def i = input.size(); i < 30000000; i++) {
+    target = hist[target] ? i - hist[target] : 0
+    hist[index] = i
+    index = target
 }
-println last
+println target
